d1c664fcce5d079ad76095f4aa4987d0f0436886
briq <990647625@qq.com>
2023-05-08 00:15:39 +0800

    feat: add partial option sets from vimcfg-private/misc.vim
    Signed-off-by: briq <990647625@qq.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,19 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.mouse='a'
+vim.opt.smartindent = true
+vim.opt.cindent = true
+vim.opt.autoindent = true
+-- vim.opt.showcmd
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.wildmenu = true
+vim.opt.scrolloff=5
+vim.opt.syntax='ON'
+vim.opt.background='dark'
+vim.opt.conceallevel=3
+vim.opt.list = true
+-- vim.opt.termguicolors to enable highlight groups
+vim.opt.termguicolors = true
