50eae1e6e46123fa88637457e7eeeb18d099c37d
briq <990647625@qq.com>
2023-05-21 00:37:07 +0800

    feat: adjust code format, temp remove coc, use nvim-lsp instead
    Signed-off-by: briq <990647625@qq.com>

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,24 +1,28 @@
 return {
-    {
-        'rmagatti/auto-session',
-        lazy = true,
-        event = 'VeryLazy',
-        dependencies = {
-
-        },
-        init = function()
+  {
+    "rmagatti/auto-session",
+    lazy = true,
+    event = "VeryLazy",
+    dependencies = {},
+    init = function() end,
+    opts = {
+      log_level = "info",
+      auto_session_enable_last_session = false,
+      -- auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
+      auto_session_enabled = true,
+      auto_save_enabled = false,
+      auto_restore_enabled = false,
+    },
+    config = true,
+    keys = {
+      {
+        mode = { "n" },
+        "<leader>Ss",
+        function()
+          require("auto-session").SaveSession()
         end,
-        opts = {
-            log_level = 'info',
-            auto_session_enable_last_session = false,
-            -- auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
-            auto_session_enabled = true,
-            auto_save_enabled = false,
-            auto_restore_enabled = false,
-        },
-        config = true,
-        keys = {
-            { mode = {"n"}, "<leader>Ss", function() require("auto-session").SaveSession() end, desc = "save-session" }
-        }
-    }
+        desc = "save-session",
+      },
+    },
+  },
 }
