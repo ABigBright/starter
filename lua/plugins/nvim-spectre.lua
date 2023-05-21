a68065a1a58447ff92a393a18864eca4e2c74926
briq <990647625@qq.com>
2023-05-21 14:55:52 +0800

    feat: add nvim-spectre
    Signed-off-by: briq <990647625@qq.com>

---
+++ lua/plugins/nvim-spectre.lua
@@ -0,0 +1,10 @@
+return {
+  -- search/replace in multiple files
+  {
+    "nvim-pack/nvim-spectre",
+    -- stylua: ignore
+    keys = {
+      { "<leader>sr", function() require("spectre").open() end, desc = "replace in files (spectre)" },
+    },
+  },
+}
