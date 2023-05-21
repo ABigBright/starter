3f8365c8f157c25688eb918b7d6b4e4dc9b87073
briq <990647625@qq.com>
2023-05-21 08:52:21 +0800

    fix: fix auto-session not auto save
    Signed-off-by: briq <990647625@qq.com>

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -10,17 +10,15 @@ return {
       auto_session_enable_last_session = false,
       -- auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
       auto_session_enabled = true,
-      auto_save_enabled = false,
-      auto_restore_enabled = false,
+      auto_save_enabled = nil,
+      auto_restore_enabled = nil,
     },
     config = true,
     keys = {
       {
         mode = { "n" },
         "<leader>Ss",
-        function()
-          require("auto-session").SaveSession()
-        end,
+        "<cmd>SessionSave<cr>",
         desc = "save-session",
       },
     },
