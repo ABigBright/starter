057dfcdd718f2cd53b678eac0e8c61441240c3d1
briq <990647625@qq.com>
2023-05-23 01:08:49 +0800

    feat: add config for auto-session, auto-session restore have some bug, need debug
    Signed-off-by: briq <990647625@qq.com>

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -4,33 +4,51 @@ return {
     lazy = true,
     event = "VeryLazy",
     dependencies = {},
-    init = function() end,
+    init = function()
+      -- vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+      -- vim.keymap.set("n", "<C-s>", require("auto-session.session-lens").search_session, {
+      --   noremap = true,
+      -- })
+    end,
     opts = {
       log_level = "info",
       auto_session_enable_last_session = false,
-      -- auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
+      auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
       auto_session_enabled = true,
       auto_save_enabled = nil,
       auto_restore_enabled = nil,
+      auto_session_suppress_dirs = nil,
+      auto_session_use_git_branch = nil,
+      -- the configs below are lua only
+      bypass_session_save_file_types = nil,
+      session_lens = {
+        -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
+      -- cwd_change_handling = { -- table: Config for handling the DirChangePre and DirChanged autocmds, can be set to nil to disable altogether
+      --   restore_upcoming_session = true, -- boolean: restore session for upcoming cwd on cwd change
+      --   pre_cwd_changed_hook = nil, -- function: This is called after auto_session code runs for the `DirChangedPre` autocmd
+      --   post_cwd_changed_hook = nil, -- function: This is called after auto_session code runs for the `DirChanged` autocmd
+      -- },
     },
     config = true,
     keys = {
-      {
-        mode = { "n" },
-        "<leader>Ss",
-        function()
-          require("auto-session").SaveSession(require("auto-session").get_root_dir(), true)
-        end,
-        desc = "save session",
-      },
-      {
-        mode = { "n" },
-        "<leader>Sl",
-        function()
-          require('auto-session.session-lens').search_session()
-        end,
-        desc = "load session",
-      },
+      -- {
+      --   "<leader>Ss",
+      --   "<cmd>SessionSave<cr>",
+      --   mode = { "n" },
+      --   desc = "save session",
+      -- },
+      -- {
+      --   "<leader>Sl",
+      --   function()
+      --     require("auto-session.session-lens").search_session(nil)
+      --   end,
+      --   mode = { "n" },
+      --   desc = "load session",
+      -- },
     },
   },
 }
