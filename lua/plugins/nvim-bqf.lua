3bf319e56dc6921ed44b969c4f94883ad75d594c
briq <990647625@qq.com>
2023-05-31 14:06:51 +0800

    feat: add nvim-bqf plugin
    Signed-off-by: briq <990647625@qq.com>

---
+++ lua/plugins/nvim-bqf.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "kevinhwang91/nvim-bqf",
+    lazy = true,
+    event = "VeryLazy",
+    dependencies = {
+      "junegunn/fzf.vim",
+    },
+    init = function() end,
+    opts = {},
+    config = true,
+    keys = {},
+  },
+}
