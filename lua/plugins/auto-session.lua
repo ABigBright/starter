8e612870482e8405c7b702b59e750af79a8687dc
briq <990647625@qq.com>
2023-10-23 17:27:02 +0800

    feat: add suppressed some path in auto-session to not save session
    Signed-off-by: briq <990647625@qq.com>

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -13,7 +13,7 @@ return {
       auto_session_enabled = true,
       auto_save_enabled = nil,
       auto_restore_enabled = nil,
-      auto_session_suppress_dirs = nil,
+      auto_session_suppress_dirs = { "~/", "~/zettelkasten/", "~/Downloads/", "/" },
       auto_session_use_git_branch = nil,
       -- the configs below are lua only
       bypass_session_save_file_types = nil,
