f6f8dc764be9dcafb402ec57955ea81b32af97c4
briq <990647625@qq.com>
2024-01-13 23:56:58 +0800

    feat: add markdown toc plugin for generate toc
    Signed-off-by: briq <990647625@qq.com>

---
+++ lua/plugins/vim-markdown-toc.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        'mzlogin/vim-markdown-toc',
+        lazy = true,
+        event = "VeryLazy",
+        init = function()
+        end,
+        opts = {
+            ---@class DefaultConfig
+        },
+        config = function(_, opts)
+        end,
+        keys = {
+        }
+    }
+}
