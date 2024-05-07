fa5c33bfc8f2f740df0320f1b88a683caeadc9d1
briq <990647625@qq.com>
2024-05-07 15:08:31 +0800

    feat: add CUS1, CUS2 tag for todo-commenter
    Signed-off-by: briq <990647625@qq.com>

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -11,7 +11,23 @@ return {
     },
     dependencies = {},
     init = function() end,
-    opts = {},
+    opts = {
+      keywords = {
+        FIX = {
+          icon = " ", -- icon used for the sign, and in search results
+          color = "error", -- can be a hex color, or a named color (see below)
+          alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+          -- signs = false, -- configure signs for some keywords individually
+        },
+        TODO = { icon = " ", color = "info" },
+        HACK = { icon = " ", color = "warning" },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+        TEST = { icon = " ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        RECD = { icon = " ", color = "info", alt = { "CUS1", "CUS2" } },
+      },
+    },
     config = true,
     keys = {
       {
@@ -49,6 +65,12 @@ return {
         desc = "hack search",
         mode = "n",
       },
+      {
+        "<leader>tsc",
+        ":TodoQuickFix keywords=",
+        desc = "cus search with keywords and cwd",
+        mode = "n",
+      }
     },
   },
 }
