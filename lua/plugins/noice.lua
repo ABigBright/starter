1608ad7fc6dd0afc21abc6d29775ae576203a93c
briq <990647625@qq.com>
2024-05-07 15:07:34 +0800

    fix: disable cmdline noice ui temporarily, because cooperated with `leaderf rg' has cursor lost problem
    Signed-off-by: briq <990647625@qq.com>

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -21,6 +21,28 @@ return {
       },
     },
     opts = {
+      cmdline = {
+        -- disable cmdline noice ui temporarily, because cooperated with `leaderf rg' has cursor lost problem.
+        enabled = false, -- enables the Noice cmdline UI
+        view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+        opts = {}, -- global options for the cmdline. See section on views
+        ---@type table<string, CmdlineFormat>
+        format = {
+          -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+          -- view: (default is cmdline view)
+          -- opts: any options passed to the view
+          -- icon_hl_group: optional hl_group for the icon
+          -- title: set to anything or empty string to hide
+          cmdline = { pattern = "^:", icon = "", lang = "vim" },
+          search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+          filter = { pattern = "^:%s!", icon = "$", lang = "bash" },
+          lua = { pattern = { "^:%slua%s+", "^:%slua%s=%s", "^:%s=%s" }, icon = "", lang = "lua" },
+          help = { pattern = "^:%she?l?p?%s+", icon = "" },
+          input = {}, -- Used by input()
+          -- lua = false, -- to disable a format, set to `false`
+        },
+      },
       lsp = {
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
